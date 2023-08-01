aae8688fab791d515ba587a91c9cf5eaf2121ad9
RKhamitov <one_RKhamitov@fortebank.com>
2023-08-01 14:16:38 +0600

    set auth to some posts routes

--- posts/router.py
+++ posts/router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from posts.schemas import PostCreate, PostUpdate
 from posts import service
+from auth.service import get_current_user
 
 router = APIRouter()
 
@@ -11,7 +12,9 @@ async def get_all_posts():
 
 
 @router.post("")
-async def create_post(post_data: PostCreate):
+async def create_post(post_data: PostCreate, user_id=Depends(get_current_user)):
+    post_data.author = user_id
+
     return await service.create_post(post_data)
 
 
@@ -21,11 +24,12 @@ async def get_post(post_id: int):
 
 
 @router.put("/{post_id}")
-async def update_post(post_id: int, post_data: PostUpdate):
+async def update_post(post_id: int, post_data: PostUpdate, user_id=Depends(get_current_user)):
+    post_data.author = user_id
     return await service.update_post(post_id, post_data)
 
 
 @router.delete("/{post_id}")
-async def delete_post(post_id: int):
+async def delete_post(post_id: int, user_id=Depends(get_current_user)):
     await service.delete_post(post_id)
     return {"id": post_id}
